3921a20a02e2706cc43bc21b3b3cb55b813c3e62
allen <allen@allendeMacBook-Pro.local>
2019-07-30 15:20:56 +0800

    add something

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,13 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
 };
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const str = 'some value';
-
-function fn(){
-    console.log('some log');
-}
+const add = (a, b) => {
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a + b;
+  }
+  
+  return NaN;
+};
+  
+module.exports = { add  };
